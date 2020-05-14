ebc2850f06e877f34ef3942a38bd9616400134ea
Alexander <Alexander.risteski23@gmail.com>
2020-05-14 13:15:44 +0200

    ReactNaming

--- src/components/teacher/teacherComponents/teacherProfile.jsx
+++ src/components/teacher/teacherComponents/teacherProfile.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-export default function teacherProfile() {
+export default function SeacherProfile() {
     return (
         <div>
             
--- src/components/teacher/teacherComponents/viewCourses.jsx
+++ src/components/teacher/teacherComponents/viewCourses.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-export default function viewCourses() {
+export default function ViewCourses() {
     return (
         <div>
             
