ec95020d3021cc32c5e5b516bbfc5f26a98eeeeb
Emil Hammarstrand <emilhammarstrand@hotmail.com>
2020-05-16 12:30:50 +0200

    You can now choose to login as teacher or student

--- src/components/logIn/logIn.jsx
+++ src/components/logIn/logIn.jsx
@@ -5,54 +5,46 @@ import { Button, Divider, Form, Grid, Segment, Radio } from 'semantic-ui-react'
 const LogIn = ({datafromLogIn}) => {
 
     const [username, setUsername] = useState("");
-    const [password, setPassword] = useState("");
-    const [student, setStudent] = useState("");
-    const [teacher, setTeacher] = useState("");
-    const [result, setResult] = useState(<div></div>)
+    const [selected, setSelected] = useState("")
 
 
-    const handleChangeStudent = event => {
-        setStudent(event.target.value);
+    const handleChange = event => {
+        setSelected(event.target.value);
     };
-    const handleChangeTeacher = event => {
-        setTeacher(event.target.value);
-    };
-    const handleChangeUsername = event => {
+    
+    /* const handleChangeUsername = event => {
         setUsername(event.target.value);
-    };
-    const handleChangePassword = event => {
-        setPassword(event.target.value);
-    };
-    /* const handleLoginBtn = () => {
-        setResult()
-    } */
+    }; */
+    const handleLoginBtn = () => {
+      console.log(selected)
+        if (selected === 'Student')
+          datafromLogIn('Student')
+        else if (selected === 'Teacher')
+        datafromLogIn('Teacher')
+    }
     
 
 return (
   <Segment placeholder className="loginForm">
-    
-        <button onClick={()=>datafromLogIn('Student')}>Student</button>
-        <button onClick={()=>datafromLogIn('Teacher')}>Teacher</button>
         <Form>
-            {/* <div className="radioBtns">
-                <Form.Input type="radio" label='Student' onChange={handleChangeStudent} ></Form.Input>
-                <Form.Input type="radio" label='Teacher' onChange={handleChangeTeacher}></Form.Input>
-            </div> */}
+            <div className="radioBtns">
+                <Form.Input type="radio" label='Student' value="Student" onChange={handleChange} checked={selected === 'Student'} ></Form.Input>
+                <Form.Input type="radio" label='Teacher' value="Teacher" onChange={handleChange} checked={selected === 'Teacher'} ></Form.Input>
+            </div>
           <Form.Input
             icon='user'
             iconPosition='left'
             label='Username'
-            onChange={handleChangeUsername}
+            /* onChange={handleChangeUsername} */
           />
           <Form.Input
             icon='lock'
             iconPosition='left'
             label='Password'
             type='password'
-            onChange={handleChangePassword}
           />
 
-        <Button className='login-btn' content='Login' primary  />
+        <Button className='login-btn' content='Login' onClick={handleLoginBtn} primary  />
         <Divider horizontal>Or</Divider>
         <Button className='sign-up-btn' content='Sign up' icon='signup' size='big' />
         </Form>
