195e4b414a0958decb6e5c560478b3c4443b504f
AngelicMob <angelic97.andersson@gmail.com>
2020-05-18 03:53:31 +0200

    added some more css and conditional rendering of components

--- src/components/teacher/teacherComponents/viewCourses.jsx
+++ src/components/teacher/teacherComponents/viewCourses.jsx
@@ -1,14 +1,57 @@
-import React from 'react';
-import {Button} from 'semantic-ui-react'
+import React,{useState} from 'react';
 import Courses from './list/courses';
-import {Icon} from "semantic-ui-react";
+import {Input,Button, Icon, Modal} from 'semantic-ui-react';
+import Calendar from 'react-calendar';
 export default function ViewCourses() {
+    const [date, setDate] = useState('2020-05-18')
+    const [modalState, setModalState] = useState(false)
+
+    function handler(e){
+        console.log(e);
+        setDate(e)
+      }
+    const addBtn = <Button onClick={()=> setModalState(true)} className="addBtn"  ><Icon icon name="add"/>Add new Assignment</Button>
+    const modalAddNew =
+  <Modal
+  trigger={addBtn}
+  centered={false}
+  open={modalState}
+  onClose={modalState}
+
+  >
+  <Modal.Header>New Assignment:</Modal.Header>
+  <Modal.Content>
+     <div className="title">
+         <h5>Title:</h5>
+         <Input className="input-title"
+                transparent
+                // icon={{ name: 'search', link: true }}
+
+
+            />
+     </div>
+     <div className="Desctiption">
+         <h5>Description:</h5>
+         <textarea id="subject" name="subject" placeholder="Write something.." ></textarea>
+     </div>
+     <div className="Deadline:">
+         <h5>Deadline:</h5>
+         <input type="date" value={date} className="date-style" onChange={(e)=>handler(e.target.value)}/>
+
+     </div>
+     <div className="button-group">
+        <Button onClick={()=> setModalState(false)} inverted color='green' className="confirmBtn"><Icon Icon name="checkmark"/>Add</Button>
+        <Button onClick={()=> setModalState(false)} className="cancelBtn"><Icon Icon name="cancel"/>Cancel</Button>
+     </div>
+
+  </Modal.Content>
+</Modal>
     return (
         <div className="view-courses">
             <div className="course-info">
                 <div className="course-info-up">
                 <h1>Javascript</h1>
-                <Button className="addBtn"><Icon Icon name="add"/>Add new Assignment</Button>
+                {modalAddNew}
                 </div>
                 <hr/>
             </div>
